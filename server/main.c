ec6f2c592f1a4a293546383553bf5f01d5637b86
Diego Ortin Fernandez <diego.ortin@estudiante.uam.es>
2020-03-05 19:14:02 +0100

    Added first files for reading the configuration from a file

--- server/main.c
+++ server/main.c
@@ -3,13 +3,44 @@
 #include <string.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <limits.h>
+#include <assert.h>
+
+#include <errno.h>
+#include <zconf.h>
 
 #include "httputils.h"
 
 #define PORT 8081
+#define DEBUG 1
+
+#define PARAM_PORT "PORT"
+#define PARAM_WEBROOT "WEBROOT"
+#define PARAM_NTHREADS "NTHREADS"
+
+#define MAX_BUFFER 1024
+#define MAX_LINE 100
+
+#define CONFIG_PATH "/server/server.cfg"
+
+struct configuration {
+	int port;
+	char webroot[MAX_BUFFER];
+	int nthreads;
+};
+
+struct configuration env;
+
+int readConfig(char *filename);
 
 int main() {
 	printf("INFO: Server started\n");
+
+	char currentdir[MAX_BUFFER];
+	getcwd(currentdir, MAX_BUFFER);
+
+	// FIXME: Does this overflow when currentdir + CONFIG_PATH > MAX_BUFFER?
+	readConfig(strcat(currentdir, CONFIG_PATH));
 
 	int socket_descriptor, new_socket, valread;
 	int opt = 1;
@@ -57,5 +88,51 @@ int main() {
 
 
 
+}
+
+int readConfig(char *filename) {
+	if (!filename) return -1;
+
+	FILE *configFile = fopen(filename, "r");
+	if (!configFile) {
+		printf("Error while opening the configuration file at %s: %s\n", filename, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+
+	char current_line[MAX_LINE];
+
+	while(fgets(current_line, MAX_LINE, configFile) != NULL) {
+		char parName[MAX_LINE], parValue[MAX_LINE];
+
+		sscanf(current_line, "%[^= ]=%s", parName, parValue);
+		if (DEBUG) {
+			printf("Read parameter with name '%s' and value '%s'\n", parName, parValue);
+			if (strcmp(parName, PARAM_PORT) == 0) {
+				long port = strtol(parValue, NULL,  10);
+				if(port > INT_MAX) { // Check if the port value can be cast to integer safely
+					printf("Incorrect port value: doesn't fit in an integer\nTerminating.\n");
+					exit(EXIT_FAILURE);
+				}
+				env.port = (int)port;
+			} else if (strcmp(parName, PARAM_WEBROOT) == 0) {
+				if((strlen(parValue) + 1) > MAX_BUFFER) {
+					printf("Parameter %s is too long.\nTerminating.\n", parName);
+					exit(EXIT_FAILURE);
+				}
+				strcpy(env.webroot, parValue);
+			} else if (strcmp(parName, PARAM_NTHREADS) == 0) {
+				if((strlen(parValue) + 1) > MAX_BUFFER) {
+					printf("Parameter %s is too long.\nTerminating.\n", parName);
+					exit(EXIT_FAILURE);
+				}
+				strcpy(env.nthreads, parValue);
+			} else {
+				printf("Unrecognized parameter: '%s'\n", parName);
+			}
+		}
+	}
+
+	return EXIT_SUCCESS;
 }
 
