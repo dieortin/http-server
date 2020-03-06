732b8e7e1af8d8731f1a2ca87cc786a4378e87b0
Diego Ortin Fernandez <diego.ortin@estudiante.uam.es>
2020-03-06 18:07:23 +0100

    Added enums with the HTTP codes

--- server/httputils.h
+++ server/httputils.h
@@ -21,4 +21,23 @@ int respond(int socket, unsigned int code, char *message, char *body);
 
 int httpreq_print(FILE *fd, struct reqStruct *request);
 
+typedef enum _HTTP_SUCCESS {
+	OK = 200,
+	CREATED = 201,
+	NO_CONTENT = 204,
+} HTTP_SUCCESS;
+typedef enum _HTTP_CLIENT_ERROR {
+	BAD_REQUEST = 400,
+	UNAUTHORIZED = 401,
+	FORBIDDEN = 403,
+	NOT_FOUND = 404,
+	METHOD_NOT_ALLOWED = 405
+} HTTP_CLIENT_ERROR;
+
+typedef enum _HTTP_SERVER_ERROR {
+	INTERNAL_ERROR = 500,
+	NOT_IMPLEMENTED = 501,
+	HTTP_VERSION_UNSUPPORTED = 505,
+} HTTP_SERVER_ERROR;
+
 #endif //PRACTICA1_HTTPUTILS_H
